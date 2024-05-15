40fe2c5127e8efe64acf3815ee83e2baa1b5521e
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-05-15 15:52:09 -0500

    Update index.css/projects.jsx
    Added more styles, some logic to projects component

--- taylor-swift-website/src/components/Projects.jsx
+++ taylor-swift-website/src/components/Projects.jsx
@@ -4,14 +4,49 @@ export default function Projects() {
     const [currentSong, setCurrentSong] = useState({
         title: ""
     })
+    const [play, setPlay] = useState(false)
+    const songArr = [
+        "SONG 1",
+        "SONG 2",
+        "SONG 3",
+        "SONG 4",
+        "SONG 5",
+        "SONG 6",
+    ]
+
+    function togglePlayPauseButton() {
+        setPlay(prev => !prev)
+    }
+
+    const renderedSongs = songArr.map((song, index) => {
+        return (
+        <div key={index} className="song-example">
+            <p>{song}</p>
+            {!play ?
+            <button onClick={togglePlayPauseButton} className="projects-song-btn play">
+                <img src="../src/assets/images/play.png"/>                            
+            </button>
+            :
+            <button onClick={togglePlayPauseButton} className="projects-song-btn pause">
+                <img src="../src/assets/images/pause.png"/>
+            </button>
+            }
+            <button className="projects-song-btn download">
+                <img src="../src/assets/images/download.png"/>
+            </button>
+        </div>
+        )
+    })
+
     return (
         <div className="projects-page">
-            {/* <img src="../src/assets/images/tayswiftfront.png" className="hero-img"/>
+            {/* <div className="projects-page-background"></div> */}
+            <img src="../src/assets/images/tayswiftfront.png" className="hero-img"/>
             <div className="border-blend-el"></div>
-            <img src="../src/assets/images/tayswiftside.png" className="hero-img"/> */}
+            <img src="../src/assets/images/tayswiftside.png" className="hero-img"/>
             <div className="opac-projects-background"></div>
             <div className="projects-container">
-                <h1>PROJECTS</h1>
+                <h1 className="projects-title">PROJECTS</h1>
                 <div className="current-song">
                     <div className="now-playing-container">
                         <p>NOW PLAYING</p>
@@ -21,13 +56,14 @@ export default function Projects() {
                         </div>
                     </div>
                     <div className="now-playing-btn-container">
-                        <button className="projects-song-btn play"><img src="../src/assets/images/play.png"/></button>
-                        <button className="projects-song-btn pause"><img src="../src/assets/images/pause.png"/></button>
-                        <button className="projects-song-btn download"><img src="../src/assets/images/download.png"/></button>
+                        <button className="projects-song-btn play-current"><img src="../src/assets/images/play.png"/></button>
+                        <button className="projects-song-btn pause-current"><img src="../src/assets/images/pause.png"/></button>
+                        <button className="projects-song-btn download-current"><img src="../src/assets/images/download.png"/></button>
                     </div>
                 </div>
                 <div className="song-list">
-                    <div className="song-example">
+                    {renderedSongs}
+                    {/* <div className="song-example">
                         <p>SONG 1</p>
                         <button className="projects-song-btn play">
                             <img src="../src/assets/images/play.png"/>                            
@@ -35,7 +71,7 @@ export default function Projects() {
                         <button className="projects-song-btn pause">
                             <img src="../src/assets/images/pause.png"/>
                         </button>
-                        <button className="projects-song-btn pause">
+                        <button className="projects-song-btn download">
                             <img src="../src/assets/images/download.png"/>
                         </button>
                     </div>
@@ -47,7 +83,7 @@ export default function Projects() {
                         <button className="projects-song-btn pause">
                             <img src="../src/assets/images/pause.png"/>
                         </button>
-                        <button className="projects-song-btn pause">
+                        <button className="projects-song-btn download">
                             <img src="../src/assets/images/download.png"/>
                         </button>
                     </div>
@@ -59,7 +95,7 @@ export default function Projects() {
                         <button className="projects-song-btn pause">
                             <img src="../src/assets/images/pause.png"/>
                         </button>
-                        <button className="projects-song-btn pause">
+                        <button className="projects-song-btn download">
                             <img src="../src/assets/images/download.png"/>
                         </button>
                     </div>
@@ -71,7 +107,7 @@ export default function Projects() {
                         <button className="projects-song-btn pause">
                             <img src="../src/assets/images/pause.png"/>
                         </button>
-                        <button className="projects-song-btn pause">
+                        <button className="projects-song-btn download">
                             <img src="../src/assets/images/download.png"/>
                         </button>
                     </div>
@@ -83,10 +119,10 @@ export default function Projects() {
                         <button className="projects-song-btn pause">
                             <img src="../src/assets/images/pause.png"/>
                         </button>
-                        <button className="projects-song-btn pause">
+                        <button className="projects-song-btn download">
                             <img src="../src/assets/images/download.png"/>
                         </button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
