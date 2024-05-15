f66f55ec3ef551dc55d381f889ba6ebf2e80bcdb
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-05-15 10:01:59 -0500

    update index.css

--- taylor-swift-website/src/components/Hero.jsx
+++ taylor-swift-website/src/components/Hero.jsx
@@ -2,8 +2,9 @@
 export default function Hero() {
     return (
         <div className="hero-section">
-            <img src="../src/assets/images/tayswiftside.png" className="hero-img"/>
             <img src="../src/assets/images/tayswiftfront.png" className="hero-img"/>
+            <div className="border-blend-el"></div>
+            <img src="../src/assets/images/tayswiftside.png" className="hero-img"/>
             <div className="filler-container"></div>
             <div className="hero-text-container">
                 <h1 className="hero-text hero-lg">THE TORTURED POETS DEPARTMENT</h1>
--- taylor-swift-website/src/components/Projects.jsx
+++ taylor-swift-website/src/components/Projects.jsx
@@ -5,38 +5,88 @@ export default function Projects() {
         title: ""
     })
     return (
-        <div className="projects-container">
-            <h1>PROJECTS</h1>
-            <div className="current-song">
-                <div className="now-playing-container">
-                    <p>NOW PLAYING</p>
-                    <div className="now-playing-image-container">
-                        <img src="../src/assets/images/previewImg.png"/>
-                        <p>SONG 1</p>
+        <div className="projects-page">
+            {/* <img src="../src/assets/images/tayswiftfront.png" className="hero-img"/>
+            <div className="border-blend-el"></div>
+            <img src="../src/assets/images/tayswiftside.png" className="hero-img"/> */}
+            <div className="opac-projects-background"></div>
+            <div className="projects-container">
+                <h1>PROJECTS</h1>
+                <div className="current-song">
+                    <div className="now-playing-container">
+                        <p>NOW PLAYING</p>
+                        <div className="now-playing-image-container">
+                            <img src="../src/assets/images/previewImg.png"/>
+                            <p>SONG 1</p>
+                        </div>
+                    </div>
+                    <div className="now-playing-btn-container">
+                        <button className="projects-song-btn play"><img src="../src/assets/images/play.png"/></button>
+                        <button className="projects-song-btn pause"><img src="../src/assets/images/pause.png"/></button>
+                        <button className="projects-song-btn download"><img src="../src/assets/images/download.png"/></button>
                     </div>
                 </div>
-                <div className="now-playing-btn-container">
-                    <button><img src="../src/assets/images/play.png"/></button>
-                    <button><img src="../src/assets/images/pause.png"/></button>
-                    <button><img src="../src/assets/images/download.png"/></button>
-                </div>
-            </div>
-            <div className="song-list">
-                <div className="song-example">
-                    <p>SONG 1</p>
-                    <button className="projects-song-btn play">
-                        <img src="../src/assets/images/play.png"/>
-                    </button>
-                </div>
-                <div className="song-example">
-                    <button className="projects-song-btn pause">
-                        <img src="../src/assets/images/pause.png"/>
-                    </button>
-                </div>
-                <div className="song-example">
-                    <button className="projects-song-btn download">
-                        <img src="../src/assets/images/download.png"/>
-                    </button>
+                <div className="song-list">
+                    <div className="song-example">
+                        <p>SONG 1</p>
+                        <button className="projects-song-btn play">
+                            <img src="../src/assets/images/play.png"/>                            
+                        </button>
+                        <button className="projects-song-btn pause">
+                            <img src="../src/assets/images/pause.png"/>
+                        </button>
+                        <button className="projects-song-btn pause">
+                            <img src="../src/assets/images/download.png"/>
+                        </button>
+                    </div>
+                    <div className="song-example">
+                        <p>SONG 2</p>
+                        <button className="projects-song-btn play">
+                            <img src="../src/assets/images/play.png"/>                            
+                        </button>
+                        <button className="projects-song-btn pause">
+                            <img src="../src/assets/images/pause.png"/>
+                        </button>
+                        <button className="projects-song-btn pause">
+                            <img src="../src/assets/images/download.png"/>
+                        </button>
+                    </div>
+                    <div className="song-example">
+                        <p>SONG 3</p>
+                        <button className="projects-song-btn play">
+                            <img src="../src/assets/images/play.png"/>                            
+                        </button>
+                        <button className="projects-song-btn pause">
+                            <img src="../src/assets/images/pause.png"/>
+                        </button>
+                        <button className="projects-song-btn pause">
+                            <img src="../src/assets/images/download.png"/>
+                        </button>
+                    </div>
+                    <div className="song-example">
+                        <p>SONG 4</p>
+                        <button className="projects-song-btn play">
+                            <img src="../src/assets/images/play.png"/>                            
+                        </button>
+                        <button className="projects-song-btn pause">
+                            <img src="../src/assets/images/pause.png"/>
+                        </button>
+                        <button className="projects-song-btn pause">
+                            <img src="../src/assets/images/download.png"/>
+                        </button>
+                    </div>
+                    <div className="song-example">
+                        <p>SONG 5</p>
+                        <button className="projects-song-btn play">
+                            <img src="../src/assets/images/play.png"/>                            
+                        </button>
+                        <button className="projects-song-btn pause">
+                            <img src="../src/assets/images/pause.png"/>
+                        </button>
+                        <button className="projects-song-btn pause">
+                            <img src="../src/assets/images/download.png"/>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
